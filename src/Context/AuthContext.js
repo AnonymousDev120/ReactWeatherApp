aa76fbbd36234f406c55e8b7fc2526fff9097468
khant21699 <khant21699@hotmail.com>
2023-05-29 22:19:59 +0630

    React Weather App

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import React from "react";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  //   const googleSignIn = () => {
+  //     const provider = new GoogleAuthProvider();
+  //     signInWithPopup(auth, provider);
+  //   };
+  const provider = new GoogleAuthProvider();
+  const [userExist, setUserExist] = useState(false);
+  const [user, setUser] = useState(null);
+  const [city, setCity] = useState(null);
+  const googleSignIn = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setUser(data.user);
+      localStorage.setItem("User", data.user);
+      setUserExist(true);
+    });
+  };
+  const SignOut = () => {
+    signOut(auth).then(() => {
+      setUserExist(false);
+      setUser(null);
+    });
+  };
+
+  const getCity = (value) => {
+    setCity(value);
+  };
+
+  useEffect(() => {
+    setUser(localStorage.getItem("User"));
+    console.log(user);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        googleSignIn,
+        user,
+        userExist,
+        SignOut,
+        city,
+        getCity,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const UseAuth = () => {
+  return useContext(AuthContext);
+};
---
+++ src/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useRef } from "react";
+import { UseAuth } from "./Context/AuthContext";
+import "./Navbar.css";
+
+const Navbar = () => {
+  var citydata = null;
+  const inputRef = useRef();
+  const { user, userExist, SignOut, getCity, city } = UseAuth();
+  const handleSignout = () => {
+    SignOut();
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getCity(citydata);
+    console.log(city);
+    inputRef.current.value = null;
+  };
+
+  return (
+    <div className="navBar">
+      <div className="profile">
+        {userExist ? (
+          <img src={user.photoURL} alt="" />
+        ) : (
+          <img
+            src="https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png"
+            alt=""
+          />
+        )}
+        {userExist ? (
+          <h1 className="user__Name">{user.displayName}</h1>
+        ) : (
+          <h1 className="user__Name">UserName</h1>
+        )}
+      </div>
+      <div className="search">
+        <input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch(e);
+            }
+          }}
+          type="text"
+          placeholder="Enter City"
+          ref={inputRef}
+          onChange={(e) => {
+            e.preventDefault();
+            citydata = e.target.value;
+          }}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <button className="signout" onClick={handleSignout}>
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
